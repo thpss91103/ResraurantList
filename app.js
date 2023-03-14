23748307deff507052985d6df92c139941322f61
thpss91103 <a0975142932@gmail.com>
2023-03-14 21:05:14 +0800

    Final version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const resturant = require("./resturant.json")
+const restaurantList = require("./restaurant.json")
 const app = express()
 const port = 3000
 
@@ -10,7 +10,20 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-  res.render('index', { resturant: resturant.results})
+  res.render('index', { restaurant: restaurantList.results})
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const restaurant = restaurantList.results.filter(restaurant => restaurant.id == req.params.id)
+  res.render('show', { restaurant: restaurant[0] })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurant = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurant: restaurant, keyword: keyword})
 })
 
 app.listen(port, () => {
