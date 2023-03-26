55f89bb0c3706f1db771f81cb3f5653c986997fb
thpss91103 <a0975142932@gmail.com>
2023-03-26 22:24:08 +0800

    feat: add sort selection

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-
 const Restaurant = require('./models/restaurant')
 const restaurantList = require("./restaurant.json")
 const methodOverride = require('method-override')
 const routes = require('./routes')
+
 const app = express()
 const port = 3000
 
@@ -16,19 +16,8 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
-app.use(routes)
 
-app.get('/search', (req, res) => {
-  if (!req.query.keywords) {
-    return res.redirect("/")
-  }
-  
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,4 +9,37 @@ router.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.get('/search', (req, res) => {
+  console.log(req.query)
+  if (!req.query.keyword) {
+    return res.redirect("/")
+  }
+
+  const keywords = req.query.keyword
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const sortMethod = req.query.sort
+  let sortKeyword = {name: 'asc'}
+
+  if (sortMethod === 'Z > A')
+    sortKeyword = {name: 'desc'}
+  
+  if (sortMethod === '類別')
+    sortKeyword = {category: 'asc'}
+
+  if(sortMethod === '地區')
+    sortKeyword = {location: 'asc'}
+    
+  return Restaurant.find({})
+    .lean()
+    .sort(sortKeyword)
+    .then(restaurant => {
+      const restaurantData = restaurant.filter(data => 
+        data.name.toLowerCase().includes(keyword) || data.category.includes(keyword)
+      )
+      res.render('index', { restaurants: restaurantData, keywords})
+    })
+    .catch(error => console.log(error))
+})
+
+
 module.exports = router
